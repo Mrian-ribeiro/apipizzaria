12bc62547173d1a1268be4702c76324cb98d91f0
Mrian-ribeiro <matheustrabalhoclt@gmail.com>
2025-10-21 17:29:26 -0300

    novas coisas

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
-// atualizar o index.js
-// , { response }
+//primeira linha do seu projeto. carrega as variaveis de ambiente antes de qualquer outro codigo
+import 'dotenv/config'
+// sintaxe de importação para todas as dependencias
 //1. Importa a ferramenta Express
-import express from 'express'
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url'; // necessario para recriar o '_dirname'
+import db from './db/db.js'; // excluir depois
+
+
+// Configuração
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = path.dirname(_filename)
+const corsOptions = {
+    origin: ['http://localhost3333', 'https://meudominio.com'],
+    methods: 'GET, POST, PUT, PATCH, DELETE',
+    credentials: true,
+};
 
 //2. cria a nossa aplicação (nosso servidor)
 const app = express()
 
 //habilita o Express para entender o formato JSON no corpo das requisiçoes
-app.use(express.json())
+// MIDDLEWARE
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+//servindo pasta public para arquivos (css,js,imagens)
+app.use(express.static(path.join(_dirname, '..','public')));
 
 //3. Define a porta em que o servidor vai 'escutar' os pedidos
-const PORTA = 3333
 
-//rota principal da aplicação
+
+//rota principal dque serve a pagina HTML
 app.get('/', (request, response) => {
     //req = Requesição (dados do pedido do cliente)
     //res = Resposta (o que vamos enviar de volta)
 
     //estamos enviando uma resposta no formato JSON
-    response.json({message: "Bem-vindo á API da Pizzaria Senac!"})
+    response.sendFile(path.join(_dirname,'..','pages','home.html'));
+})
+
+// --TRATAMENTO DE ERROS --
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('algo deu errado no servidor!')
 })
 
 //4. manda o servidor ficar "escutando" na porta definida
+const PORTA = process.env.PORT || 3333;
 app.listen(PORTA, () => {
-    console.log(`Servidor rodando na porta ${PORTA}. acesse http://localhost:${PORTA}`)
+    console.log(`Servidor rodando na porta ${PORTA}`)
 })
 
 //seus dados mockados (simulando o banco de dados)
